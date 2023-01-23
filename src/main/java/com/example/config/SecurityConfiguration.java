915218d69e07870b65696c4b9bd2b9a848ebe309
Sandhya Bairi <sandhya.bairi154@gmail.com>
2023-01-23 22:27:13 +0530

    added config server

---
+++ src/main/java/com/example/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .ignoringAntMatchers("/encrypt/**")
+                .ignoringAntMatchers("/decrypt/**");
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
+    }
+}
